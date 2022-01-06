594210de8ca9471bb9bfd2bd6832f6b31cc72c55
Lucia Heredia <LuciaHeredia22@gmail.com>
2022-01-06 22:11:01 +0200

    MaterialDesign transition applied

--- app/src/main/java/com/example/avengersendgamecharacters/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/avengersendgamecharacters/fragments/DetailsFragment.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import com.example.avengersendgamecharacters.R
 import com.example.avengersendgamecharacters.databinding.FragmentDetailsBinding
 import com.example.avengersendgamecharacters.model.Character
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-
+import com.google.android.material.transition.MaterialContainerTransform
 
 class DetailsFragment : Fragment() {
 
@@ -29,9 +26,11 @@ class DetailsFragment : Fragment() {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        sharedElementEnterTransition = MaterialContainerTransform()
+
         val displayCharacter = args.character // get data from HomeFragment
 
-        // convert json to Character model
+        // convert jsonString to Character model
         val mapper = jacksonObjectMapper()
         val characterDetails: Character = mapper.readValue(displayCharacter, Character::class.java)
 
--- app/src/main/java/com/example/avengersendgamecharacters/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/avengersendgamecharacters/fragments/HomeFragment.kt
@@ -5,36 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.avengersendgamecharacters.R
 import com.example.avengersendgamecharacters.adapter.DataAdapter
 import com.example.avengersendgamecharacters.data.DataSource
 import com.example.avengersendgamecharacters.databinding.FragmentHomeBinding
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.google.android.material.card.MaterialCardView
 
 class HomeFragment : Fragment(){
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
         val myDataset = DataSource().loadCharacters()
         binding.recyclerview.layoutManager = LinearLayoutManager(activity) // vertical layout Manager
         val adapter = DataAdapter(this.requireContext(), myDataset)
         binding.recyclerview.adapter = adapter // Setting Adapter with recyclerview
         adapter.setOnItemClickListener(object : DataAdapter.OnItemClickListener{
             override fun onItemClick(position: Int) {
+
+                // convert Character model to jsonString
                 val mapper = jacksonObjectMapper()
                 val jsonString = mapper.writeValueAsString(myDataset[position])
-                val action = HomeFragmentDirections.actionHomeFragmentToDetailsFragment(jsonString)
-                Navigation.findNavController(binding.root).navigate(action)
+
+                // MaterialDesign Transition + SafeArgs + Navigation
+                val cardView = view.findViewById<MaterialCardView>(R.id.details_view)
+                val transitionName = getString(R.string.character_card_transition)
+                val extras = FragmentNavigatorExtras(cardView to transitionName)
+                val directions = HomeFragmentDirections.actionHomeFragmentToDetailsFragment(jsonString)
+                findNavController().navigate(directions,extras)
+
             }
         })
 
-        return binding.root
+        return view
     }
 
 }
