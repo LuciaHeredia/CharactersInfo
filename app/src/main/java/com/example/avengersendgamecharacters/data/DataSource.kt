38fa6180753aa75f6a6dfbc13ded2934f3bc44ca
Lucia Heredia <LuciaHeredia22@gmail.com>
2022-01-06 06:31:40 +0200

    data entered

--- app/src/main/java/com/example/avengersendgamecharacters/data/DataSource.kt
+++ app/src/main/java/com/example/avengersendgamecharacters/data/DataSource.kt
@@ -13,16 +13,12 @@ class DataSource {
             Character(R.drawable.antman_5, R.drawable.antman_55, R.string.char5, R.string.char5role ),
             Character(R.drawable.captain_marvel_6, R.drawable.captain_marvel_66, R.string.char6, R.string.char6role ),
             Character(R.drawable.hulk_7, R.drawable.hulk_77, R.string.char7, R.string.char7role ),
-
-
-            Character(R.drawable.captain_america_2, R.drawable.tony_1, R.string.char2, R.string.char2role ),
-
-            Character(R.drawable.captain_america_2, R.drawable.tony_1, R.string.char2, R.string.char2role ),
-
-            Character(R.drawable.captain_america_2, R.drawable.tony_1, R.string.char2, R.string.char2role ),
-
-            Character(R.drawable.captain_america_2, R.drawable.tony_1, R.string.char2, R.string.char2role )
-
+            Character(R.drawable.hawkeye_8, R.drawable.hawkeye_88, R.string.char8, R.string.char8role ),
+            Character(R.drawable.war_machine_9, R.drawable.war_machine_99, R.string.char9, R.string.char9role ),
+            Character(R.drawable.nebula_10, R.drawable.nebula_1010, R.string.char10, R.string.char10role ),
+            Character(R.drawable.okoye_11, R.drawable.okoye_1111, R.string.char11, R.string.char11role ),
+            Character(R.drawable.rocket_12, R.drawable.rocket_1212, R.string.char12, R.string.char12role ),
+            Character(R.drawable.wong_13, R.drawable.wong_1313, R.string.char13, R.string.char13role )
         )
     }
 }
